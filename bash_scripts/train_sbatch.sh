4e8fae1963f9fe1695c2efcf8a8a09992b7b9db1
Rishabh Gupta <rgupta2@cori07.nersc.gov>
2019-08-06 15:05:16 -0700

    bash_scripts/rbc_train.sh

--- bash_scripts/train_sbatch.sh
+++ bash_scripts/train_sbatch.sh
@@ -21,6 +21,4 @@ pip install tensorflow-datasets
 
 #run the application:
 python  /global/cscratch1/sd/rgupta2/backup/StyleGAN/src/StyleGAN-Tensorflow/main.py --dataset FFHQ_128 --img_size 128 --gpu_num 4 --progressive True --phase train --checkpoint_dir ../stored_outputs/FFHQ_128_with_1200k_iteratrion_new/checkpoint --result_dir ../stored_outputs/FFHQ_128_with_1200k_iteratrion_new/result --log_dir ../stored_outputs/FFHQ_128_with_1200k_iteratrion_new/log --sample_dir ../stored_outputs/FFHQ_128_with_1200k_iteratrion_new/sample 	
-
-
 # python  /global/cscratch1/sd/rgupta2/backup/StyleGAN/src/StyleGAN-Tensorflow/main.py --dataset rbc_2000  --checkpoint_dir ../stored_outputs/rbc_2000/checkpoint --result_dir ../stored_outputs/rbc_2000/result --log_dir ../stored_outputs/rbc_2000/log --sample_dir ../stored_outputs/rbc_2000/sample   --img_size 128 --gpu_num 2 --progressive True --phase train
