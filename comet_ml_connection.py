16f655f686bf5f758ecd5ce1208829a7733ae3ed
Rishabh Gupta <rgupta2@cori05.nersc.gov>
2019-10-23 13:27:35 -0700

    modified readme

---
+++ comet_ml_connection.py
@@ -0,0 +1,21 @@
+
+
+from websocket import create_connection
+
+ws = create_connection("wss://www.comet.ml/ws/logger-ws")
+
+print("Sending 'Hello, World'...")
+
+ws.send("Hello, World")
+
+print("Sent")
+
+print("Receiving...")
+
+result =  ws.recv()
+
+print("Received '%s'" % result)
+
+ws.close()
+
+
